66fee6291e1f017178113b764317c3d8998c55a2
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-11-30 16:39:28 +0100

    Main.cpp now tests wordnet.isKnown() and wordnet.areSynonyms().

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <Python.h>
 #include <iostream>
 #include <string>
-#include "WordnetReader.h"
-#include "WordnetReader.cpp"
+
+#include "wordnet_reader.h"
+#include "wordnet_reader.cpp"
 
 int main() {
   Py_Initialize();
@@ -10,15 +11,20 @@ int main() {
   PyObject* curDir = PyUnicode_FromString(".");
   PyList_Append(sysPath, curDir);
   Py_DECREF(curDir);
+  
   std::string word;
   std::string anotherWord;
+  Wordnet::Reader wordnet = Wordnet::Reader();
   std:: cout << "Podaj pierwsze słowo: \n";
   std::getline(std::cin, word);
-  WordnetReader::Synset synset = WordnetReader::Synset(word);
-  std::cout << synset.getLemma() << std::endl;
+  if (!wordnet.isKnown(word))
+    std::cout<<"The word is not in the dictionary!"<<std::endl;
   std::cout << "Podaj drugie słowo: \n";
   std::getline(std::cin, anotherWord);
-  std::cout << synset.isSynonymOf(anotherWord);
+  if (!wordnet.isKnown(anotherWord))
+    std::cout<<"The word is not in the dictionary!"<<std::endl;
+  if (wordnet.areSynonyms(anotherWord))
+    std::cout<<"These words are synonyms."<<std::endl;https://www.youtube.com/watch?v=rCRP-5om_3Y
   Py_Finalize();
   return 0;
 }
