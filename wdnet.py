bb6b5e4772c0a51af0f7ca25fcfa17f752dba048
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-02 10:10:14 +0100

    Modifies get_synonyms() so that it returns a list of unique values and changes get_closure to get_hyponyms

--- wdnet.py
+++ wdnet.py
@@ -2,15 +2,18 @@
 from nltk.corpus import wordnet as wn
 
 def is_known(word):
-  return bool(wn.synsets(word))
+  return wn.synsets(word)
 
 def get_synonyms(word):
   synsets = wn.synsets(word)
-  return list([l for s in synsets for l in s.lemma_names()])
+  synonyms = [l for s in synsets for l in s.lemma_names()]
+  return list(synonyms)
 
 def are_synonyms(word1, word2):
   return word2 in get_synonyms(word1)
 
-def get_closure(word, relation, level):
+def get_hyponyms(word, level):
   synsets = wn.synsets(word)
-  return set([l for synset in synsets for s in synset.closure(relation, level) for l in s.lemma_names()])  
+  closure = [synset.closure(lambda s:s.hyponyms(), level) for synset in synsets]
+  hyponyms = set([lemma for lemma in synset.lemma_names() for synset in closure])
+  return list(hyponyms)
