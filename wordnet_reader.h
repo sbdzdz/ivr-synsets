98e8d17bf108be53af12fee015fc49b0a4ed5721
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-02 01:49:09 +0100

    Removes unnecessary fields from the header file.

--- wordnet_reader.h
+++ wordnet_reader.h
@@ -10,13 +10,9 @@ namespace Wordnet
 
   class Reader
   {
-  private:
-    PyObject * moduleName;
-    PyObject* moduleObject;
-    PyObject* isKnown;
   public:
     Reader();
-    bool isKnown(std::string word);
+    bool isKnown(std::string& word);
     bool areSynonyms(std::string word, std::string otherWord);
     std::vector<std::string> getSynonymsOf(std::string word);
     std::vector<std::string> getHyponymsOf(std::string word, int level);
