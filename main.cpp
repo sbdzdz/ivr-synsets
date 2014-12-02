001b41c6846a4f8d6dd443118de74f5c6fa95b55
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-02 01:37:59 +0100

    Implements the main program loop to test isKnown() and areSynonyms()

--- main.cpp
+++ main.cpp
@@ -5,26 +5,37 @@
 #include "wordnet_reader.h"
 #include "wordnet_reader.cpp"
 
-int main() {
+int main() 
+{
   Py_Initialize();
   PyObject* sysPath = PySys_GetObject((char*)"path");
   PyObject* curDir = PyUnicode_FromString(".");
+  // PyObject* curDir = PyString_FromString(".");
   PyList_Append(sysPath, curDir);
   Py_DECREF(curDir);
-  
+
   std::string word;
-  std::string anotherWord;
+  std::string otherWord;
   Wordnet::Reader wordnet = Wordnet::Reader();
-  std:: cout << "Podaj pierwsze słowo: \n";
-  std::getline(std::cin, word);
-  if (!wordnet.isKnown(word))
-    std::cout<<"The word is not in the dictionary!"<<std::endl;
-  std::cout << "Podaj drugie słowo: \n";
-  std::getline(std::cin, anotherWord);
-  if (!wordnet.isKnown(anotherWord))
-    std::cout<<"The word is not in the dictionary!"<<std::endl;
-  if (wordnet.areSynonyms(word, anotherWord))
-    std::cout<<"These words are synonyms."<<std::endl;
+
+  while (true) {
+    std::cout<<"Podaj pierwsze słowo: \n";
+    std::getline(std::cin, word);
+    if (!wordnet.isKnown(word)) {
+      std::cout<<word<<" is not in the dictionary!"<<std::endl;
+      continue;
+    }
+    std::cout<<"Podaj drugie słowo: \n";
+    std::getline(std::cin, otherWord);
+    if (!wordnet.isKnown(otherWord)) {
+      std::cout<<otherWord<<" is not in the dictionary!"<<std::endl;
+      continue;
+    }
+    if (wordnet.areSynonyms(word, otherWord))
+      std::cout<<"These words are synonyms."<<std::endl;
+    else
+      std::cout<<"These words are not synonymous"<<std::endl;
+  }
   Py_Finalize();
   return 0;
 }
