95b15938da1a98bb000f924fb934bb84bddf780c
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-05 01:16:48 +0100

    Implements getHyponymsOf, removes Py_DECREFs after borrowed references.

--- wordnet_reader.cpp
+++ wordnet_reader.cpp
@@ -21,7 +21,6 @@ bool Wordnet::Reader::isKnown(std::string& word) {
   Py_DECREF(args);
 
   const int result = PyObject_IsTrue(resultObj);
-  Py_DECREF(resultObj);
   return (bool)result;
 }
 
@@ -39,12 +38,10 @@ bool Wordnet::Reader::areSynonyms(std::string word, std::string otherWord) {
   Py_DECREF(args);
 
   const int result = PyObject_IsTrue(resultObj);
-  Py_DECREF(resultObj);
   return (bool)result;
 }
 
-//std::vector<std::string> Wordnet::Reader::getSynonymsOf(std::string word) {
-void Wordnet::Reader::getSynonymsOf(std::string word) {
+std::vector<std::string> Wordnet::Reader::getSynonymsOf(std::string word) {
   PyObject* moduleName = PyUnicode_FromString(Wordnet::MODULE_NAME);
   PyObject* moduleObject = PyImport_Import(moduleName);
   Py_DECREF(moduleName);
@@ -58,17 +55,43 @@ void Wordnet::Reader::getSynonymsOf(std::string word) {
   Py_DECREF(args);
 
   Py_ssize_t i, n;
-  PyObject* synonym;
+  PyObject* listItem;
+  std::vector<std::string> synonyms;
 
   n = PyList_Size(resultObj);
   for (i = 0; i < n; i++) {
-      synonym = PyList_GetItem(resultObj, i);
-      PyObject* pyStr = PyUnicode_AsEncodedString(synonym, "utf-8", "Error ~");
-      Py_DECREF(synonym);
-      std::cout<<PyBytes_AS_STRING(pyStr)<<std::endl;
+      listItem = PyList_GetItem(resultObj, i);
+      PyObject* pyStr = PyUnicode_AsEncodedString(listItem, "utf-8", "Error ~");
+      Py_DECREF(listItem);
+      synonyms.push_back(PyBytes_AS_STRING(pyStr));
   }
+  return synonyms;
 }
 
 std::vector<std::string> Wordnet::Reader::getHyponymsOf(std::string word, int level) {
-  //TODO
+  PyObject* moduleName = PyUnicode_FromString(Wordnet::MODULE_NAME);
+  PyObject* moduleObject = PyImport_Import(moduleName);
+  Py_DECREF(moduleName);
+
+  PyObject* args = Py_BuildValue("(si)", word.c_str(), level);
+  PyObject* getHyponymsOf = PyObject_GetAttrString(moduleObject, "get_hyponyms");
+  Py_DECREF(moduleObject);
+
+  PyObject* resultObj = PyObject_CallObject(getHyponymsOf, args);
+  Py_DECREF(getHyponymsOf);
+  Py_DECREF(args);
+
+  Py_ssize_t i, n;
+  PyObject* listItem;
+  std::vector<std::string> hyponyms;
+
+  n = PyList_Size(resultObj);
+  for (i = 0; i < n; i++) {
+      listItem = PyList_GetItem(resultObj, i);
+      PyObject* pyStr = PyUnicode_AsEncodedString(listItem, "utf-8", "Error ~");
+      Py_DECREF(listItem);
+      hyponyms.push_back(PyBytes_AS_STRING(pyStr));
+  }
+  return hyponyms;
 }
+
