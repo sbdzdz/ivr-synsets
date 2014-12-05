db8702d7ed9a0239f639166d9233d5f1e2135511
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2014-12-05 01:17:56 +0100

    Updates the main loop so that it tests getSynonymsOf() and getHyponymsOf(). Adds a way to exit the main while loop

--- main.cpp
+++ main.cpp
@@ -5,37 +5,54 @@
 #include "wordnet_reader.h"
 #include "wordnet_reader.cpp"
 
+void printVector(std::vector<std::string> vec) {
+    for(std::vector<std::string>::const_iterator i = vec.begin(); i != vec.end(); ++i)
+	std::cout<<*i<<" ";
+    std::cout<<"\n";
+}
+
 int main() 
 {
   Py_Initialize();
   PyObject* sysPath = PySys_GetObject((char*)"path");
   PyObject* curDir = PyUnicode_FromString(".");
-  // PyObject* curDir = PyString_FromString(".");
   PyList_Append(sysPath, curDir);
   Py_DECREF(curDir);
 
   std::string word;
   std::string otherWord;
+  int level = 3;
   Wordnet::Reader wordnet = Wordnet::Reader();
 
+  std::vector<std::string> synonyms;
+  std::vector<std::string> hyponyms;
+
   while (true) {
-    std::cout<<"Podaj pierwsze słowo: \n";
+    std::cout<<"Enter a word (or type \"exit\" to leave)\n";
     std::getline(std::cin, word);
+    if (word == "exit") {
+	break;
+    }
     if (!wordnet.isKnown(word)) {
-      std::cout<<word<<" is not in the dictionary!"<<std::endl;
+      std::cout<<"Oops, "<<word<<" is not in the dictionary!"<<std::endl;
       continue;
     }
-    wordnet.getSynonymsOf(word);
-    std::cout<<"Podaj drugie słowo: \n";
+    synonyms = wordnet.getSynonymsOf(word);
+    std::cout<<"Synonyms: \n";
+    printVector(synonyms);
+    hyponyms = wordnet.getHyponymsOf(word, level);
+    std::cout<<"Hyponyms: \n";
+    printVector(hyponyms);
+    std::cout<<"Enter another word: \n";
     std::getline(std::cin, otherWord);
     if (!wordnet.isKnown(otherWord)) {
-      std::cout<<otherWord<<" is not in the dictionary!"<<std::endl;
+      std::cout<<"Oops, "<<otherWord<<"  is not in the dictionary!"<<std::endl;
       continue;
     }
     if (wordnet.areSynonyms(word, otherWord))
-      std::cout<<"These words are synonyms."<<std::endl;
+	std::cout<<"Yes, "<<word<<" and "<<otherWord<<" are synonyms.\n";
     else
-      std::cout<<"These words are not synonymous"<<std::endl;
+	std::cout<<"No, "<<word<<" and "<<otherWord<<" are not synonyms.\n";
   }
   Py_Finalize();
   return 0;
