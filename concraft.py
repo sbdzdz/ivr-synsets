c6ef29ae1f96d2b995b4ed3e6f505dd484ceb28b
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2015-01-09 14:46:40 +0100

    Encloses function in ConcraftServer and ConcraftClient classes.

--- concraft.py
+++ concraft.py
@@ -1,29 +1,69 @@
 import os
 import re
+import socket
 import subprocess
 import time
 
-def tag(sentence):
-    with open('input', 'w') as input:
+PATH_TO_CONCRAFT = '~/.cabal/bin'
+
+class ConcraftServer:
+    def __initialize__(self):
+        self.port = self.find_free_port()
+        self.call = self.generate_call()
+        self.server = self.start_server()
+
+    def __del__(self):
+        self.server.terminate()
+
+    def find_free_port(self):
+        socket = socket.socket()
+        socket.bind(('', 0))
+        port = socket.getsockname()[1]
+        return port
+
+    def generate_call(self):
+        call = "{0}/concraft-pl server {0}/model.gz --port {1}".format(PATH_TO_CONCRAFT, self.port)
+        return call
+
+    def start_server(self):
+        server = subprocess.Popen(self.call, shell=True)
+        return server
+        
+    def get_port(self):
+        return self.port
+
+class ConcraftClient:
+    def __initialize__(self, port):
+        self.port = port
+        self.call = self.generate_call()
+
+    def lemmatize(self, sentence):
+        self.write_to_file(sentence)
+        serverRunning = False
+        while (not serverRunning):
+            try:
+                concraftOutput = subprocess.check_output(self.call, shell=True)
+                serverRunning = True
+            except subprocess.CalledProcessError:
+                time.sleep(5)
+        return self.parse(concraftOutput.decode('utf-8'))
+
+    def generate_call(self):
+        call = "{0}concraft-pl client --port {1} < input".format(PATH_TO_CONCRAFT, self.port)
+        return call
+
+    def write_to_file(self, sentence):
+        with open('input', 'w') as input:
         input.write(sentence)
-    call = "~/.cabal/bin/concraft-pl client < input"
-    serverRunning = False
-    while (not serverRunning):
-        try:
-            subprocess.check_call(call, shell=True)
-            serverRunning = True
-        except subprocess.CalledProcessError:
-            time.sleep(5)
-    os.remove('input')
 
-def parse(concraftOutput):
-    sentence = [line.split()[0] for line in concraftOutput if disambiguation(line)]
-    return ''.join(sentence)
+    def parse(concraftOutput):
+        sentence = [line.split()[0] for line in concraftOutput.split("\n") if disambiguation(line)]
+        return ' '.join(sentence)
 
-def disambiguation(line):
-    return line.split()[-1] == 'disamb'
+    def disambiguation(line):
+        return line and line.split()[-1] == 'disamb'
 
-def start_concraft_server():
-    call = "~/.cabal/bin/concraft-pl server ~/.cabal/bin/model.gz"
-    server = subprocess.Popen(call, shell = True)
-    return server
+
+    
+
+
