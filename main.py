dad44efc83df259e86b3b0b86d5274cc06867662
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2015-01-09 16:16:29 +0100

    Adds the Wordnet part.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import concraft
+import wdnet
+
+def analyse(sentence, concraftClient):
+    for word in concraftClient.lemmatize(sentence).split():
+        print(word)
+        synonyms = wdnet.get_synonyms(word)
+        for synonym in set(synonyms):
+            print ("\t{0}".format(synonym))
 
 server = concraft.Server()
 port = server.get_port()
 client = concraft.Client(port)
 
 sentence = input("Please wait for the server to load and then enter a sentence\n")
-print(client.lemmatize(sentence))
-
+analyse(sentence, client)
 while (True):
     sentence = input("Please enter another sentence or leave a blank line to exit:\n")
     if not sentence:
         break
-    print(client.lemmatize(sentence))
+    analyse(sentence, client)
