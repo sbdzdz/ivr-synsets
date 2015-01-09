239cc1504767f572e1b1b611b81674ab84141ac7
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2015-01-09 16:26:42 +0100

    Changes synonyms to hyponyms.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import wdnet
 def analyse(sentence, concraftClient):
     for word in concraftClient.lemmatize(sentence).split():
         print(word)
-        synonyms = wdnet.get_synonyms(word)
-        for synonym in set(synonyms):
-            print ("\t{0}".format(synonym))
+        synonyms = wdnet.get_hyponyms(word, 2)
+        for hyponym in set(hypoonyms):
+            print ("\t{0}".format(hyponym))
 
 server = concraft.Server()
 port = server.get_port()
